08c950927ed88489e56bae6903e6fcb2ee5c48f4
s4rdh0n0 <s4rdh0n0@gmail.com>
2020-09-12 16:17:11 +0700

    s4rdh0n0

--- model/office.py
+++ model/office.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 # Model
 from model.base import BaseModel
@@ -11,10 +12,51 @@ class OfficeModel(BaseModel):
     def __init__(self, host="", token=""):
         super().__init__(host=host, token=token)
 
-    def get_all(self):
-        response = requests.get('{}/{}'.format(self.host, self.root), headers=self.header)
-        return response
+    def schema(self):
+        return requests.get('{}/{}/schema'.format(self.host, self.root), headers=self.header)
 
-    def find(self, officeid=""):
+    def all(self):
+        return requests.get('{}/{}'.format(self.host, self.root), headers=self.header)
+
+    def kkpTolocal(self, officeid="", kkp={}, actived=False):
+        result = self.schema().json()['result']
+        result['_id'] = officeid
+        result['code'] = kkp['code']
+        result['officetypeid'] = kkp['officetypeid']
+        result['parent'] = kkp['parent']
+        result['nama'] = kkp['name']
+        result['kota'] = kkp['city']
+        result['alamat'] = kkp['address']
+        result['phone'] = kkp['phone']
+        result['email'] = kkp['email']
+        result['fax'] = kkp['fax']
+        result['actived'] = actived
+
+        return result
+
+    def count(self, typeid="", officeid=""):
+        param = 'typeid={}&officeid={}'.format(typeid, officeid)
+        return requests.get('{}/{}/count?{}'.format(self.host, self.root, param), headers=self.header)
+
+    def find(self, typeid="", officeid=""):
+        param = 'typeid={}&officeid={}'.format(typeid, officeid)
+        return requests.get('{}/{}/find?{}'.format(self.host, self.root, param), headers=self.header)
+
+    def all_kkp(self):
+        return requests.get('{}/{}/allkkp'.format(self.host, self.root), headers=self.header)
+
+    def kkp(self, officeid=""):
         param = 'officeid={}'.format(officeid)
-        return requests.get('{}/{}/find?{}'.format(self.host, self.root,param), headers=self.host)
+        return requests.get('{}/{}/kkp?{}'.format(self.host, self.root, param), headers = self.header)
+
+    def add(self, office={}):
+        djson = {'office': office}
+        return requests.post('{}/{}'.format(self.host, self.root), json=djson, headers=self.header)
+
+    def update(self, office={}):
+        djson = {'office': office}
+        return requests.put('{}/{}'.format(self.host, self.root), json=djson, headers=self.header)
+
+    def delete(self, officeid=""):
+        djson = {'officeid': officeid}
+        return requests.delete('{}/{}'.format(self.host, self.root), json=djson, headers=djson)
--- testing.py
+++ testing.py
@@ -1,15 +1,49 @@
 import requests
 
-def TestingUser():
+
+# Tornado
+import tornado.gen
+
+# Model
+from model.office import OfficeModel
+
+def auth():
     auth_param = {
         'username': 's4rdh0n0',
         'password': '4231Dodon'
     }
     auth = requests.post('{}/{}/{}'.format("http://localhost:8000", 'auth', 'login'), json=auth_param)
-    token = auth.json()['token']
-
-    print(token)
-    # user = UserModel(officeid="", host="http://localhost:8000", token=token)
+    return auth.json()['token']
 
 
-TestingUser()
+@tornado.gen.coroutine
+def TestingOffice():
+    token = auth()
+    office = OfficeModel(host="http://localhost:8000", token=token)
+    # for o in office.all().json()['result']:
+    #     print("Data     : {}".format(o))
+
+    for o in office.all_kkp().json()['result']:
+        # print("Data     : {}".format(o))
+        child = office.kkp(officeid=o['officeid'])
+
+        tornado.gen.sleep(0.5)
+
+        conveter = office.kkpTolocal(o['officeid'], child.json()['result'], False)
+        print("Data     : {}".format(conveter))
+
+
+        tornado.gen.sleep(0.5)
+
+        add = office.add(conveter)
+        print(add.json()['result'])
+        
+
+    count = office.count().json()['result']
+    print(count)
+
+    schema = office.schema().json()['result']
+    print(schema)
+
+
+TestingOffice()
